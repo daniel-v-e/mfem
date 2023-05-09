320ae6798a50166480e133487565b4b07ec1518b
Sebastian Grimberg <sjg@amazon.com>
2023-05-09 16:43:15 -0700

    Add AddMult and AddMultTranspose methods for BilinearFormExtension objects
    Also clarify parallel + conforming prolongation operator in ParFiniteElementSpace, fixing a bug for conforming meshes with shared triangles for diagonal assembly with partial assembly.

--- fem/pfespace.cpp
+++ fem/pfespace.cpp
@@ -976,12 +976,9 @@ void ParFiniteElementSpace::Build_Dof_TrueDof_Matrix() const // matrix P
 
 const Operator *ParFiniteElementSpace::GetProlongationMatrix() const
 {
-   if (Conforming())
+   if (Conforming() && !nd_strias)
    {
       if (Pconf) { return Pconf; }
-
-      if (nd_strias) { return Dof_TrueDof_Matrix(); }
-
       if (NRanks == 1)
       {
          Pconf = new IdentityOperator(GetTrueVSize());
