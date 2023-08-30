1ada8074490b4917268f348d9e4abad110a84bdc
daniel-v-e <dvoneschwege@gmail.com>
2023-08-30 21:47:49 +0200

    Subprocess loggign

--- examples/runall.py
+++ examples/runall.py
@@ -1,30 +1,39 @@
 import os
 import glob
-import re  # Import the regex module
+import re
+import subprocess  # Needed to capture the command output
+import logging
 
 # change dir to examples
 os.chdir("examples")
 
-def run_command(command):
-    print(f"\nRunning command: {command}")
-    os.system(command)
+# Configure the logging module
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def run_command(command, logfile):
+    logging.info(f"Running command: {command}")
+    
+    # Capture the output and error of the command
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    # Log the output to the specified logfile
+    with open(logfile, 'a') as f:
+        f.write(result.stdout.decode())
+        f.write(result.stderr.decode())
 
 def extract_sample_run(line):
-    # Use regex to match the pattern: ex* -m ../data/*.mesh
     match = re.search(r"(ex\d+ -m \.\./data/[\w-]+\.mesh)", line)
     if match:
         return match.group(1)
     return None
 
 def custom_sort(filename):
-    # Extracting the number from the filename
     match = re.search(r'ex(\d+)', filename)
     if match:
         return int(match.group(1))
-    return float('inf')  # return a large number for non-matching filenames
+    return float('inf')
 
 def main():
-    # Find all cpp files in the current directory
     filepaths = sorted(glob.glob('*.cpp'), key=custom_sort)
     filepaths = [f for f in filepaths if not f.endswith('p.cpp')]
     for filepath in filepaths:
@@ -32,22 +41,22 @@ def main():
         basename, _ = os.path.splitext(filename)
         
         # Run make command
-        run_command(f"make {basename}")
+        run_command(f"make {basename}", f"{basename}.make.log")
         
         # Extract and run the sample command from the cpp file
+        command = None
         with open(filepath, 'r') as file:
             for line in file:
                 command = extract_sample_run(line)
                 if command:
                     command = f"./{command}"
-                    run_command(command)
-                    break  # Exit the loop once the command is found
-            
+                    run_command(command, f"{basename}.log")
+                    break
+        
         if not command:
-            print(f"Failed to find the sample command in {filename}")
+            logging.warning(f"Failed to find the sample command in {filename}")
             continue
 
-        # Rename the output files
         if os.path.exists("sol.gf"):
             os.rename("sol.gf", f"{basename}.gf")
         if os.path.exists("mesh.mesh"):
@@ -55,11 +64,10 @@ def main():
         if os.path.exists("refined.mesh"):
             os.rename("refined.mesh", f"{basename}_refined.mesh")
         
-        # Identify mesh name from the command
         mesh_name = command.split("-m")[1].split()[0] if "-m" in command else "mesh.mesh"
         
         # Run the glvis command
-        run_command(f"~/glvis-4.2/glvis -m {mesh_name} -g {basename}.gf")
+        run_command(f"~/glvis-4.2/glvis -m {mesh_name} -g {basename}.gf", f"{basename}.glvis.log")
 
 if __name__ == "__main__":
     main()
